4c832905b2d47aec6acf08fed46814e6a3794aab
JoniMora <yoniiran@gmail.com>
2023-10-22 20:05:36 -0300

    Shopping cart

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
