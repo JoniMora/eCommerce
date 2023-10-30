6b4a47e16c718c6bf6fc07a6eb32d37fa9689ae2
JoniMora <yoniiran@gmail.com>
2023-10-30 00:48:00 -0300

    Paypal

--- frontend/src/api/orders.ts
+++ frontend/src/api/orders.ts
@@ -2,6 +2,6 @@ import { Order } from "../Interfaces";
 import { authAxios } from "./useAxios";
 
 
-export const createOrder =async (data: Order) => {
+export const create_order =async (data: Order) => {
     await authAxios.post('/orders/create/', data)
 }
